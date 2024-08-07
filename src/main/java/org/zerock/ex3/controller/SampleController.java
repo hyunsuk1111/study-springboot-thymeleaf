0042131357e7a559b961f69954c584611d48a6f3
hyunsuk1111 <epsxkf@gmail.com>
2024-08-07 23:47:42 +0900

    feat : 링크 및 레이아웃
    링크 및 레이아웃 추가

--- src/main/java/org/zerock/ex3/controller/SampleController.java
+++ src/main/java/org/zerock/ex3/controller/SampleController.java
@@ -4,13 +4,14 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.ex3.dto.SampleDTO;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import java.util.stream.LongStream;
 
 @Controller
@@ -23,8 +24,8 @@ public class SampleController {
         log.info("ex1........");
     }
 
-    @GetMapping({"/ex2"})
-    public String exModel1(Model model) {
+    @GetMapping({"/ex2", "/exLink"})
+    public void exModel1(Model model) {
         List<SampleDTO> list = LongStream.rangeClosed(1, 20)
                 .mapToObj(i -> {
                     SampleDTO dto = SampleDTO.builder()
@@ -37,7 +38,38 @@ public class SampleController {
                 }).collect(Collectors.toList());
 
         model.addAttribute("list", list);
+    }
 
-        return "sample/ex2";
+    @GetMapping({"/exInline"})
+    public String exInline(RedirectAttributes redirectAttributes) {
+        SampleDTO dto = SampleDTO.builder()
+                .sno(100L)
+                .first("First..100")
+                .last("Last..100")
+                .regTime(LocalDateTime.now())
+                .build();
+
+        redirectAttributes.addFlashAttribute("result", "success");
+        redirectAttributes.addFlashAttribute("dto", dto);
+
+        return "redirect:/sample/ex3";
+    }
+
+    @GetMapping("/ex3")
+    public String ex3() {
+        return "sample/ex3";
+    }
+
+    @GetMapping("/exLayout1")
+    public String exLayout1() {
+        return "sample/exLayout1";
+    }
+
+    //PathVariable로 맵핑
+    @GetMapping("/exView/{sno}")
+    public String exView(@PathVariable Long sno)  {
+        System.out.println("sno = " + sno);
+
+        return "sample/ex1";
     }
 }
