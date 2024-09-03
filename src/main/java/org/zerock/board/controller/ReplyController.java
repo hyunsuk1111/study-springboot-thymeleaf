91f663f91f06fe2a94d8f047059b82cdb4a16cd4
hyunsuk1111 <epsxkf@gmail.com>
2024-09-03 18:23:04 +0900

    FEAT : 댓글 조회 기능 추가
    댓글 조회 기능 추가

--- src/main/java/org/zerock/board/controller/ReplyController.java
+++ src/main/java/org/zerock/board/controller/ReplyController.java
@@ -3,7 +3,6 @@ package org.zerock.board.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
