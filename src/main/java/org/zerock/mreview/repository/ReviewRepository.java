00a5aaa0f72b3ef64634ace78fe328fae8ef6de2
hyunsuk1111 <epsxkf@gmail.com>
2024-09-16 16:16:04 +0900

    Revert "소스트리 커밋 테스트"
    This reverts commit e0cf24fca800fd20fe2b90f2cb334ca02678192b.

--- src/main/java/org/zerock/mreview/repository/ReviewRepository.java
+++ src/main/java/org/zerock/mreview/repository/ReviewRepository.java
@@ -3,5 +3,5 @@ package org.zerock.mreview.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.zerock.mreview.entity.Review;
 
-public interface ReviewRepository extends JpaRepository<Review, Integer> {
+public interface ReviewRepository extends JpaRepository<Review, Long> {
 }
