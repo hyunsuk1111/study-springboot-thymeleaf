38b16cab527e5479053cf3f6b2a7a632c9ff0b15
hyunsuk1111 <epsxkf@gmail.com>
2024-09-16 16:09:31 +0900

    Revert "FEAT : 리뷰 리포지토리 추가"
    This reverts commit 9fd1f7fa1823d5589eecf5a175aa8e0dd857e8b0.

--- src/main/java/org/zerock/mreview/repository/ReviewRepository.java
+++
@@ -1,7 +0,0 @@
-package org.zerock.mreview.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.zerock.mreview.entity.Review;
-
-public interface ReviewRepository extends JpaRepository<Review, Long> {
-}
